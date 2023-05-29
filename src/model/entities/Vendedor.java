70845b820ef91a08ffbe13e947bb6b9fa33a2527
João <joaoleonardo9921@gmail.com>
2023-05-29 13:23:28 -0300

    adicionando entity e dao (inacabado)

---
+++ src/model/entities/Vendedor.java
@@ -0,0 +1,11 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Vendedor {
+    private Integer id;
+    private String nome;
+    private String email;
+    private Date dtAniversario;
+    private Double salarioBase;
+}
