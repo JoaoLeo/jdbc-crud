5a20b9a3195641da08e6cb7ae045d6ab6f26b46f
João <joaoleonardo9921@gmail.com>
2023-05-29 13:27:45 -0300

    arrumando departamento e implementando serializable

--- src/model/entities/Departamento.java
+++ src/model/entities/Departamento.java
@@ -1,8 +1,9 @@
 package model.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Departamento {
+public class Departamento implements Serializable {
     private Integer id;
     private String nome;
 
@@ -31,16 +32,28 @@ public class Departamento {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Departamento that = (Departamento) o;
-        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Departamento other = (Departamento) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nome);
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
     @Override
