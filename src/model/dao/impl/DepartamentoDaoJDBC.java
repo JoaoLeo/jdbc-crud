0ce3b95f8156dc011c8a4b41988354c927bac565
João <joaoleonardo9921@gmail.com>
2023-05-30 10:07:43 -0300

    adicionando find by id de departamento e testando

--- src/model/dao/impl/DepartamentoDaoJDBC.java
+++ src/model/dao/impl/DepartamentoDaoJDBC.java
@@ -1,16 +1,23 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartamentoDao;
 import model.entities.Departamento;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DepartamentoDaoJDBC  implements DepartamentoDao {
     private Connection conn;
+
     public DepartamentoDaoJDBC(Connection conn) {
         this.conn = conn;
     }
+
     @Override
     public List<Departamento> getAll() {
         return null;
@@ -33,6 +40,27 @@ public class DepartamentoDaoJDBC  implements DepartamentoDao {
 
     @Override
     public Departamento findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * from departamento d where id = ?"
+            );
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Departamento d = new Departamento();
+                d.setId(rs.getInt("Id"));
+                d.setNome(rs.getString("Nome"));
+                return d;
+            }
+
+        } catch (SQLException err) {
+            throw new DbException(err.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
         return null;
     }
 }
--- test/model/dao/DepartamentoDaoTest.java
+++ test/model/dao/DepartamentoDaoTest.java
@@ -1,15 +1,22 @@
 package model.dao;
 
 import model.entities.Departamento;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 public class DepartamentoDaoTest {
+    private  static  DepartamentoDao departamentoDao = null;
+    @BeforeAll
+    static void setup(){
+        departamentoDao = DaoFactory.criaDepartamentoDao();
+    }
 
     @Test
-    void getAll(){
-        DepartamentoDao departamentoDao = DaoFactory.criaDepartamentoDao();
-        assert (departamentoDao.getAll().size() != 0);
+    void findById(){
+        Departamento d =  departamentoDao.findById(4);
+        System.out.println("Departamento: " + d.toString());
+        assert (d != null);
     }
 }
